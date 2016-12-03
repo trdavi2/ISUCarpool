3ebc061b453ce545e455adcd6528f92f2442ee4f
trdavi2 <trdavi2@ilstu.edu>
2016-12-03 16:01:52 -0600

    message fragment

--- app/src/main/java/com/it326/isucarpool/MessageFragment.java
+++ app/src/main/java/com/it326/isucarpool/MessageFragment.java
@@ -4,18 +4,33 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.it326.isucarpool.model.Chat;
+import com.it326.isucarpool.model.Message;
+
+import java.util.ArrayList;
 
 
 public class MessageFragment extends Fragment
 {
     private Button send;
+    private DatabaseReference messages;
     private EditText input;
     private String chatId;
+    private FirebaseListAdapter<Message> adapter;
+    private ListView messageList;
+
     interface MessageFragmentListener{
 
     }
@@ -34,7 +49,38 @@ public class MessageFragment extends Fragment
         chatId = getArguments().getString("chatId");
         input = (EditText) v.findViewById(R.id.input);
         input.setText(chatId);
+        messages = FirebaseDatabase.getInstance().getReference("chats").child(chatId).child("messages");
         return v;
+    }
+
+    private void displayChatMessages()
+    {
+        //final ListView messages = (ListView) findViewById(R.id.list_of_messages);
+        adapter = new FirebaseListAdapter<Message>(this.getActivity(), Message.class, R.layout.message, messages) {
+
+            @Override
+            protected void populateView(View v, Message model, int position) {
+                messageList = (ListView) v.findViewById(R.id.list_of_messages);
+                // Get references to the views of message.xml
+                TextView messageText = (TextView) v.findViewById(R.id.message_text);
+                //TextView messageUser = (TextView) v.findViewById(R.id.message_user);
+                //TextView messageTime = (TextView) v.findViewById(R.id.message_time);
+
+                messageText.setText(model.getMessageText());
+
+                // Set their text
+                //ArrayList<Message> chatMessage = model;
+                // Message lastMessage = chatMessages.get(chatMessages.size() - 1);
+                //messageText.setText(lastMessage.getMessageText());
+                //messageUser.setText(lastMessage.getMessageUser());
+
+                // Format the date before showing it
+                //messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                //lastMessage.getMessageTime()));
+            }
+
+        };
+        messageList.setAdapter(adapter);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
