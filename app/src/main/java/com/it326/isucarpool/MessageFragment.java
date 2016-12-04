a6d0a2ed7996f69d6bdf19012eab5e50123c265f
trdavi2 <trdavi2@ilstu.edu>
2016-12-03 19:03:28 -0600

    message layout

--- app/src/main/java/com/it326/isucarpool/MessageFragment.java
+++ app/src/main/java/com/it326/isucarpool/MessageFragment.java
@@ -1,22 +1,30 @@
 package com.it326.isucarpool;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.it326.isucarpool.model.Chat;
 import com.it326.isucarpool.model.Message;
 
@@ -31,6 +39,9 @@ public class MessageFragment extends Fragment
     private String chatId;
     private FirebaseListAdapter<Message> adapter;
     private ListView messageList;
+    private String currUserId;
+    private String otherUser;
+    private Chat currChat;
 
     interface MessageFragmentListener{
 
@@ -47,18 +58,37 @@ public class MessageFragment extends Fragment
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_message, container, false);
         // Inflate the layout for this fragmen
+        currUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         chatId = getArguments().getString("chatId");
         input = (EditText) v.findViewById(R.id.input);
-        input.setText(chatId);
+
         messages = FirebaseDatabase.getInstance().getReference("chats").child(chatId).child("messages");
         messageList = (ListView) v.findViewById(R.id.list_of_messages);
+        FirebaseDatabase.getInstance().getReference("chats").child(chatId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                currChat = dataSnapshot.getValue(Chat.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+/*
+        if(currUserId.equals(currChat.getDriverId())){
+            otherUser = currChat.getRiderId();
+        } else {
+            otherUser = currChat.getDriverId();
+        }
+*/
         displayChatMessages();
 
         send = (Button) v.findViewById(R.id.send_button);
         send.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                messages.push().setValue(new Message(input.getText().toString(), FirebaseAuth.getInstance().getCurrentUser().getUid()));
+                messages.push().setValue(new Message(input.getText().toString(), currUserId));
                 input.setText("");
             }
         });
@@ -79,8 +109,28 @@ public class MessageFragment extends Fragment
                 TextView messageText = (TextView) v.findViewById(R.id.message_text);
                 //TextView messageUser = (TextView) v.findViewById(R.id.message_user);
                 //TextView messageTime = (TextView) v.findViewById(R.id.message_time);
-
+/*
+                LinearLayout.LayoutParams leftMessage = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                leftMessage.setMargins(5, 5, 25, 5);
+                LinearLayout.LayoutParams rightMessage = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                rightMessage.setMargins(25, 5, 5, 5);
+*/
                 messageText.setText(model.getMessageText());
+
+                if(model.getMessageUser().equals(currUserId)) {
+                    //messageText.setLayoutParams(rightMessage);
+                    messageText.setGravity(Gravity.END);
+                    messageText.setPaddingRelative(25, 5, 5, 5);
+                    //messageText.setBackgroundColor(Color.BLUE);
+                    //messageText.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
+                } else {
+                   // messageText.setLayoutParams(leftMessage);
+                    messageText.setGravity(Gravity.START);
+                    messageText.setPaddingRelative(5, 5, 25, 5);
+                    messageText.setBackgroundColor(Color.BLUE);
+                }
+
+
 
                 // Set their text
                 //ArrayList<Message> chatMessage = model;
